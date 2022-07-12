880c00c1597bf1c3727393e62a623bdd33da962f
Rishab Arora <29125087+grobo021@users.noreply.github.com>
2022-07-12 17:29:06 +0530

    improved website model; added sdg

--- init.py
+++ init.py
@@ -1,55 +1,64 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import urllib.request
-import numpy as np
-from werkzeug.utils import secure_filename
 import os
 
+import numpy as np
+from flask import Flask, render_template, request, redirect, flash, send_from_directory
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from werkzeug.utils import secure_filename
+
 app = Flask(
-  __name__,
-  static_folder='./upload'
+    __name__,
+    static_folder='./upload'
 )
 
-model = load_model('./model/covid19_classification.h5')
+model = load_model('./model')
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/images/<path:path>")
+def send_image(path):
+    return send_from_directory("templates/images", path)
+
 @app.route("/", methods=["POST"])
 def submit_file():
-  if request.method == 'POST':
-    if 'file' not in request.files:
-      flash('No file part')
-      return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-      flash('No file selected for uploading')
-      return redirect(request.url)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
 
-    if file:
-      global path
-      filename = secure_filename(file.filename)
-      path = os.path.join('upload', filename)
-      file.save(path)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
 
-      img = image.load_img(path, target_size=(150,150))
-      x = image.img_to_array(img)
-      x = np.expand_dims(x,axis=0)
-      images = np.vstack([x])
-      classes = model.predict(images,batch_size=10)
+        if file:
+            if os.path.exists('upload'):
+                os.rmdir('upload')
 
-      if classes == 0:
-        label = 'COVID19'
-      else:
-        label = 'Normal'
+            os.mkdir('./upload')
+            filename = secure_filename(file.filename)
+            path = os.path.join('upload', filename)
+            file.save(path)
 
-      flash(label)
-      flash(path)
-      return redirect('/')
+            img = image.load_img(path, target_size=(150, 150))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            images = np.vstack([x])
+            classes = model.predict(images, batch_size=10)
+
+            if classes == 0:
+                label = 'COVID19'
+            else:
+                label = 'Normal'
+
+            flash(label)
+            flash(path)
+            return redirect('/')
+
 
 # start the flask app, allow remote connections
 if __name__ == "__main__":
-  app.secret_key = 'never gonna give you up never gonna let you down never gonna desert you'
-  app.run(host='0.0.0.0')
+    app.secret_key = 'EEEEEEEEEEEEEEEEEEEEEEEEEEEEE'
+    app.run(host='0.0.0.0')
